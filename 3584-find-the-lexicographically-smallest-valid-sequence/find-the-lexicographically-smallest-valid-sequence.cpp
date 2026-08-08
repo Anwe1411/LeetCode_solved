33c919eca9f86133979879c2c5ac897561094cfa
Anwesha Mohapatra <anwesha.mohapatra1411@gmail.com>
2026-08-08 17:36:35 +0530

    Time: 41 ms (69.15%) | Memory: 115.7 MB (15.96%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        vector<int>ans;
+        int N=word1.size();
+        int M=word2.size();
+        int R=M-1;
+        int c=0;
+        vector<int>Right(N);
+        for(int i=N-1;i>=0;i--){
+            Right[i]=c;
+            if(R>=0&&word1[i]==word2[R]){
+                R--;
+                c++;
+            }
+        }
+         bool changed = false;
+        int j = 0; // pointer for word2
+
+        for (int i = 0; i < N && j < M; i++) {
+            if (word1[i] == word2[j]) {
+                ans.push_back(i);
+                j++;
+            } else if (!changed && Right[i] >= M - 1 - j) {
+                // Spend our one allowed substitution here
+                ans.push_back(i);
+                j++;
+                changed = true;
+            }
+        }
+
+        if (j == M) {
+            return ans;
+        }
+        return {};
+    
+    }
+};
