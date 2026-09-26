a05b5423609bae9540e5d94248d3d8ecfe02501b
Anwesha Mohapatra <anwesha.mohapatra1411@gmail.com>
2026-09-26 22:53:58 +0530

    Time: 90 ms (40.50%) | Memory: 122.8 MB (46.73%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> mp;
+        // Store key-value pairs
+        for (auto &pair : knowledge) 
+            mp[pair[0]] = pair[1];
+        string ans;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != '(') 
+                ans += s[i];
+            else {
+                // Find closing bracket
+                int j = i + 1;
+                while (s[j] != ')') 
+                    j++;
+                // Extract key
+                string key = s.substr(i + 1, j - i - 1);
+                // Replace with value or '?'
+                if (mp.find(key) != mp.end()) 
+                    ans += mp[key];
+                 else 
+                    ans += '?';
+                // Skip everything through ')'
+                i = j;}}
+        return ans;
+    }
+};
